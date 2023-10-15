15d1cdf6e966d86e7a4d31fa8ac3b7b6130355d6
Shiva1903 <124800474+Shiva1903@users.noreply.github.com>
2023-10-15 18:41:25 +0530

    first commit

--- addSub.js
+++
@@ -1,8 +0,0 @@
-let add=(num1,num2)=>{
-    return num1+num2;
-}
-let sub=(num1,num2)=>{
-    return num1-num2;
-}
-let Name="Shiva"
-module.exports={add,sub,Name}
--- index.js
+++
@@ -1,6 +0,0 @@
-let events=require("node:events")
-let emitter=new events()
-emitter.on("Order-Pizza",()=>{
-    console.log("Order recieved. Please wait")
-})
-emitter.emit("Order-Pizza")
